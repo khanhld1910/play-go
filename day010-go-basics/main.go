dcf3b273a1d1802aaef8b0d9b293f1ebab3479aa
Khanh Luong <khanhld1910@gmail.com>
2024-10-18 10:34:35 +0700

    if else

--- day010-go-basics/main.go
+++ day010-go-basics/main.go
@@ -9,4 +9,31 @@ func main() {
 	for i, v := range pow {
 		fmt.Printf("2**%d = %d\n", i, v)
 	}
+
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	// if without else
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
+
+	// if with a statement before condition
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else {
+		fmt.Println(num, "is positive")
+	}
+
+	// if with multiple conditions
+	if num := 10; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
 }
